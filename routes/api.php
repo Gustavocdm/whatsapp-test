ecc483787807ed83995714b400e2bf636f1313ab
Gustavo-BVR <gustavo.melo@bvr.com.br>
2024-09-05 10:51:09 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/test', function (Request $request) {
+    return $request->all();
+});
+
+Route::get('/webhooks', function (Request $request) {
+    $challenge = $request->get('hub_challenge');
+    $tokenEnviado = $request->get('hub_verify_token');
+
+    $token = env('WHATSAPP_WEBHOOK_VERIFY_TOKEN');
+
+    if ($token == $tokenEnviado) {
+        return response([
+            "error" => "Token doens't match."
+        ], 400);
+    }
+    
+    return response([
+        "hub.challenge" => $challenge
+    ]);
+});
